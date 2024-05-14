ab6198b03b337435c7dff4338f624c1204def515
Adrian Rambal <adrian.rambal@gmail.com>
2024-05-14 12:16:48 +0800

    bulma io scss added

--- frontend/src/components/ChooseSendReceive.vue
+++ frontend/src/components/ChooseSendReceive.vue
@@ -1,6 +1,10 @@
 
 <template>
     <p>Hello world</p>
+    <div class="buttons">
+          <a class="button is-primary">Primary</a>
+          <a class="button is-link">Link</a>
+        </div>
 </template>
 
 <!-- <script setup>
@@ -8,3 +12,7 @@ import { ref } from "vue";
 
 const date = ref();
 </script> -->
+
+<style lang="scss">
+    @forward "~bulma/sass/elements/button";
+</style>
